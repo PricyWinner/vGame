4f61c26548cccaab4b685f08056bd888b103ac78
PricyWinner <alex.imanuel.28@gmail.com>
2023-01-13 11:40:46 +0700

    Initial Commit

---
+++ vGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vGame
+//
+//  Created by Alexander Imanuel on 13/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
